acd331008d10373e7f9e4d3239fe098b2f6836e0
Gonzalo Othacehe <gonza.otc@gmail.com>
2025-08-27 18:15:28 -0300

    reduce failing test code just to hook deploy

--- src/mocks/ReHypothecationMock.sol
+++ src/mocks/ReHypothecationMock.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
+pragma solidity ^0.8.24;
 
 import {ReHypothecationHook} from "src/general/ReHypothecationHook.sol";
 import {BaseHook} from "src/base/BaseHook.sol";
